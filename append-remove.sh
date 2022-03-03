a28c33fa2c9fc2abe4a0839444fdc54a64212bc8
bug3 <bug3dev@gmail.com>
2022-03-03 19:03:08 +0300

    Define appendText func

--- append-remove.sh
+++ append-remove.sh
@@ -5,3 +5,15 @@ textFile=appendices
 tempFile=.temp.txt
 
 cp $destFile $destFile.bak
+
+appendText () {
+    wordCount=$([[ -f $destFile ]] && cat $destFile | wc -w)
+
+    ed -s $destFile <<< w
+    
+    if [[ $wordCount -eq 0 ]]; then
+        tail -n +2 $textFile > $destFile
+    else
+        cat $textFile >> $destFile
+    fi    
+}
